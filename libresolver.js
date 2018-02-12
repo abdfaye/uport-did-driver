c29f1f459bf67244c97d5e590390e55a1047452d
abdfaye <a-faye@hotmail.fr>
2018-02-12 16:49:33 +0100

    Settings

--- libresolver.js
+++ libresolver.js
@@ -3,7 +3,7 @@ const uportLiteGet = UportLite({
   ipfsGw: 'https://ipfs.infura.io/ipfs/',
   infuraKey: 'INFURA_API_KEY',
   networks: {
-    '0x3': {
+    '0x4': {
       rpcUrl: 'https://rinkeby.infura.io',
       address: '0x2cc31912b2b0f3075a87b3640923d45a26cef3ee'
     }
